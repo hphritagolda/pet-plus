ae9ce48c85e35cd73dd6fe66f3cf3b679cbc7de6
hphritagolda <107784033+hphritagolda@users.noreply.github.com>
2023-05-21 13:41:49 +0800

    Added icons

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,3 +1,6 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { UserIcon } from "@heroicons/react/24/solid";
+import { ArrowUpOnSquareIcon } from "@heroicons/react/24/solid";
 import { ReactNode } from "react";
 
 export function LightButton(props: { children: ReactNode }) {
@@ -12,6 +15,45 @@ export function DarkButton(props: { children: ReactNode }) {
   return (
     <button className="text-center text-pink-500 rounded-full bg-slate-900 py-2 w-full max-w-xs font-bold">
       {props.children}
+    </button>
+  );
+}
+
+export function ExitButton(props: { children: ReactNode }) {
+  return (
+    <button
+      type="button"
+      className="bg-pink-500 rounded-full p-2 inline-flex items-center justify-center text-blue-500 hover:text-pink-500 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-500"
+    >
+      <span className="sr-only">Close menu</span>
+
+      <XMarkIcon className="w-6 h-6" />
+    </button>
+  );
+}
+
+export function ProfileButton(props: { children: ReactNode }) {
+  return (
+    <button
+      type="button"
+      className="bg-pink-500 rounded-full p-2 inline-flex items-center justify-center text-blue-500 hover:text-pink-500 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-500"
+    >
+      <span className="sr-only">Close menu</span>
+
+      <UserIcon className="w-6 h-6" />
+    </button>
+  );
+}
+
+export function UploadButton(props: { children: ReactNode }) {
+  return (
+    <button
+      type="button"
+      className="bg-pink-500 rounded-full p-2 inline-flex items-center justify-center text-blue-500 hover:text-pink-500 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-500"
+    >
+      <span className="sr-only">Close menu</span>
+
+      <ArrowUpOnSquareIcon className="w-6 h-6" />
     </button>
   );
 }
--- src/pages/pet/new.tsx
+++ src/pages/pet/new.tsx
@@ -2,7 +2,13 @@ import { ReactNode } from "react";
 import Image from "next/image";
 import petPlus from "@/assets/petplus.svg";
 import andrewProfile from "@/assets/andrew.png";
-import { DarkButton, LightButton } from "@/components/Buttons";
+import {
+  DarkButton,
+  ExitButton,
+  LightButton,
+  ProfileButton,
+  UploadButton,
+} from "@/components/Buttons";
 
 function Title(props: { children: ReactNode }) {
   return (
@@ -23,59 +29,67 @@ function Subtitle(props: { children: ReactNode }) {
 export default function NewPetPage() {
   return (
     <div className="px-8 mt-20">
-      <div className="flex flex-col items-center gap-3 mb-4">
-        <div className="flex flex-row items-center gap-16 mb-7">
-          <Image src={petPlus} alt="" width={50} height={50} />
-          <Title>PetPlus</Title>
-          <Image
-            src={andrewProfile}
-            alt=""
-            width={50}
-            height={50}
-            className="rounded-full w-10 h-10 object-cover"
-          />
-        </div>
+      <div className="fixed top-8 left-8 right-8 flex justify-between">
+        <ExitButton>Exit</ExitButton>
+        <ProfileButton>User Profile</ProfileButton>
       </div>
-      <div className="flex flex-col items-center gap-4 bg-grey-500 rounded-lg py-5">
-        <Subtitle>New Account</Subtitle>
-        <div className="flex col">
-          <form className="w-full px-4">
-            <input
-              type="text"
-              className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
-              placeholder="First Name"
-            />
-          </form>
-          <form className="w-full px-4">
-            <input
-              type="text"
-              className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
-              placeholder="Last Name"
-            />
-          </form>
+      <div className="flex items-center justify-center h-64 ">
+        <UploadButton>Upload Photo</UploadButton>
+      </div>
+      <div className="absolute inset-x-0 bottom-0 gap-4 bg-grey-500 rounded-lg py-5">
+        <Subtitle>New Pet Details</Subtitle>
+        <form className="grid grid-cols-2 gap-2 px-4 mt-12">
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="First Name"
+          />
+
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="Last Name"
+          />
+
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="Pet Type"
+          />
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="Gender"
+          />
+
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="Breed"
+          />
+
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="Age"
+          />
+
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="Height"
+          />
+
+          <input
+            type="text"
+            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
+            placeholder="Weight"
+          />
+        </form>
+
+        <div className="mt-8 flex items-center justify-center">
+          <LightButton>Add New Pet</LightButton>
         </div>
-        <form className="w-full px-4">
-          <input
-            type="text"
-            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
-            placeholder="E-mail"
-          />
-        </form>
-        <form className="w-full px-4">
-          <input
-            type="text"
-            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
-            placeholder="Password"
-          />
-        </form>
-        <form className="w-full px-4">
-          <input
-            type="text"
-            className="rounded-lg bg-grey-500 border-pink-500 border-4 py-2 px-4 text-pink-500 block w-full placeholder:text-pink-500"
-            placeholder="Confirm Password"
-          />
-        </form>
-        <LightButton>Create Account</LightButton>
       </div>
     </div>
   );
