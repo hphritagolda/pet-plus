8afb0fab713b177203b648c22378426b5cdf5398
hphritagolda <107784033+hphritagolda@users.noreply.github.com>
2023-05-24 20:59:09 +0800

    Made pet profile view better

--- app/routes/pet.$petId.tsx
+++ app/routes/pet.$petId.tsx
@@ -42,53 +42,55 @@ export default function PetPage() {
   const { pet } = useLoaderData<typeof loader>();
   return (
     <div className="">
-      <div className="fixed inset-x-8 top-8 z-10 flex justify-between md:left-[28rem]">
+      <div className="fixed inset-x-8 top-8 z-10 flex justify-between md:right-[30rem]">
         <ExitLink to="/" />
         <ProfileLink to="/menu"></ProfileLink>
       </div>
-      <div className="fixed inset-x-0 top-0 h-[50vh] md:bottom-0 md:left-96 md:h-full">
-        <img
-          src={poppyProfile}
-          alt=""
-          width={500}
-          height={500}
-          className="absolute inset-0 h-full w-full object-cover object-center"
-        />
-        <div className="absolute inset-x-0 bottom-16 text-center">
-          <PetNameTitle>
-            {pet.firstName} {pet.lastName}
-          </PetNameTitle>
-          <PetSubtitle>{pet.breed}</PetSubtitle>
-          <PetDescription>
-            {pet.gender} | {pet.age} y.o | {pet.height}cm | {pet.weight}kg
-          </PetDescription>
+      <div className="fixed inset-0 flex flex-col flex-nowrap md:flex-row">
+        <div className="relative -mb-4 h-64 md:-mr-4 md:mb-0 md:h-full md:flex-1">
+          <img
+            src={poppyProfile}
+            alt={pet.firstName}
+            width={500}
+            height={500}
+            className="absolute inset-0 -z-10 h-full w-full object-cover object-center"
+          />
+          <div className="absolute inset-x-0 bottom-6 text-center">
+            <PetNameTitle>
+              {pet.firstName} {pet.lastName}
+            </PetNameTitle>
+            <PetSubtitle>{pet.breed}</PetSubtitle>
+            <PetDescription>
+              {pet.gender} | {pet.age} y.o | {pet.height}cm | {pet.weight}kg
+            </PetDescription>
+          </div>
         </div>
-      </div>
-      <div className="fixed inset-x-0 bottom-0 flex h-[50vh] flex-col flex-nowrap rounded-t-lg bg-grey-500 px-4 py-5 md:left-0 md:right-auto md:top-0 md:h-full md:max-w-md md:rounded-r-lg md:rounded-tl-none">
-        <div className="mb-4 flex flex-row justify-between rounded-full bg-blue-500">
-          <NavLink
-            to="."
-            className="-mr-2 rounded-full px-8 py-4 text-pink-500 [&.active]:bg-pink-500 [&.active]:text-blue-500"
-            end
-          >
-            Overview
-          </NavLink>
-          <NavLink
-            to="vaccinations"
-            className="-mx-2 rounded-full px-8 py-4 text-pink-500 [&.active]:bg-pink-500 [&.active]:text-blue-500"
-          >
-            Vaccinations
-          </NavLink>
-          <NavLink
-            to="records"
-            className="-ml-2 rounded-full px-8 py-4 text-pink-500 [&.active]:bg-pink-500 [&.active]:text-blue-500"
-          >
-            Records
-          </NavLink>
-        </div>
+        <div className="flex min-h-0 flex-1 flex-col flex-nowrap rounded-t-xl bg-grey-500 px-4 py-5 shadow-2xl shadow-black md:max-w-md md:flex-auto md:rounded-l-xl md:rounded-tr-none">
+          <div className="mb-4 flex flex-row justify-between rounded-full bg-blue-500">
+            <NavLink
+              to="."
+              className="-mr-2 rounded-full px-6 py-3 text-pink-500 [&.active]:bg-pink-500 [&.active]:text-blue-500"
+              end
+            >
+              Overview
+            </NavLink>
+            <NavLink
+              to="vaccinations"
+              className="-mx-2 rounded-full px-6 py-3 text-pink-500 [&.active]:bg-pink-500 [&.active]:text-blue-500"
+            >
+              Vaccinations
+            </NavLink>
+            <NavLink
+              to="records"
+              className="-ml-2 rounded-full px-6 py-3 text-pink-500 [&.active]:bg-pink-500 [&.active]:text-blue-500"
+            >
+              Records
+            </NavLink>
+          </div>
 
-        <div className="flex-1 overflow-y-auto">
-          <Outlet />
+          <div className="flex-1 overflow-y-auto">
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
